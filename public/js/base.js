fc56feb5bf1ce49d33a02093b4176787bb021060
liuweigg <liuweigg@126.com>
2015-06-26 16:19:23 +0800

    Update base.js

--- public/js/base.js
+++ public/js/base.js
@@ -96,6 +96,10 @@ function contentEffects(){
   }
 }
 function toggleDuoshuoComments(container){
+    var ck = document.getElementById('comment-box');
+    if(ck != null){
+      return true ;
+    }
     var el = document.createElement('div');//该div不需要设置class="ds-thread"
     el.setAttribute('data-thread-key', '{{ page.id }}');//必选参数
     el.setAttribute('data-url', '{{ site.url }}{{ page.url }}');//必选参数
